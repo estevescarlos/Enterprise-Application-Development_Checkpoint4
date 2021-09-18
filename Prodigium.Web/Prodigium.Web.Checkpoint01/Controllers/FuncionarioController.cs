1951c7c6251f399104d4d2b89aa8e88640c52f94
estevescarlos <carlosheenry@gmail.com>
2021-09-18 15:47:03 -0300

    Atualização de projeto.

---
+++ Prodigium.Web/Prodigium.Web.Checkpoint01/Controllers/FuncionarioController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Prodigium.Web.Checkpoint01.Models;
+
+namespace Prodigium.Web.Checkpoint01.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private static List<Funcionario> _funcionarios = new List<Funcionario>();
+        private static int _codigo = 1;
+        
+        [HttpPost]
+        public IActionResult Cadastrar(Funcionario funcionario)
+        {
+            return Redirect("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            CarregarNiveis();
+            return View();
+        }
+
+        private void CarregarNiveis()
+        {
+            var lista = new List<string>(new string[] { "Estagiário", "Auxiliar", "Assistente", "Analista", "Supervisor", "Gerente" });
+            ViewBag.niveis = new SelectList(lista);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prodigium.Web/Prodigium.Web.Checkpoint01/Models/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prodigium.Web.Checkpoint01.Models
+{
+    public class Funcionario
+    {
+        [HiddenInput]
+        public int Codigo { get; set; }
+        
+        public string Nome { get; set; }
+        
+        public string CPF { get; set; }
+        
+        public Setores Setor { get; set; }
+        
+        public string Cargo { get; set; }
+        
+        public string Nivel { get; set; }
+
+        [Display(Name = "Data de Admissão"), DataType(DataType.Date)]
+        public DateTime DataAdmissao { get; set; }
+        
+        [Display(Name = "Salário")]
+        public decimal Salario { get; set; }
+        
+        public bool Status { get; set; }
+    }
+
+    public enum Setores
+    {
+        Administrativo, Desenvolvimento, Financeiro, Comercial, Marketing, Suporte, Qualidade 
+    }
+}
